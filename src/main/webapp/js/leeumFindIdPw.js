940b582cb88478f3635bf2e144fed66134fce8b4
chlsnfl <125331839+chlsnfl@users.noreply.github.com>
2023-05-24 18:33:52 +0900

    find id and password

---
+++ src/main/webapp/js/leeumFindIdPw.js
@@ -0,0 +1,19 @@
+function findIdPw(){
+	const url = "/Leeum/findIdPw.jsp";
+	const width = 700, height = 700;
+	 
+	let left = (document.body.offsetWidth / 2) - (width / 2);
+	let top = (document.body.offsetHeight / 2) - (height / 2);
+	left += window.screenLeft;
+	
+	window.open(
+		url, 
+		"popup", 
+		`width=${width}, 
+		 height=${height}, 
+		 left=${left}, 
+		 top=${top}`
+    );
+}
+
+
